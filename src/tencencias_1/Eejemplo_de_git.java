8a25aaf3237fd69dfdd6d20965cc78222a0a8a5d
Estefania Zhungo <gloria.zhungo.est@tecazuay.edu.ec>
2024-11-29 22:01:52 -0500

    Añadí una nueva clase

---
+++ src/tencencias_1/Eejemplo_de_git.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tencencias_1;
+
+/**
+ *
+ * @author estef
+ */
+public class Eejemplo_de_git {
+    //Ejemplo de git tefis
+}
